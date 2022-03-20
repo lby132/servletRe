275c9421667a58a3a7aebd3e75656529e571be41
lby132 <quddbscjswow@naver.com>
2022-03-20 23:17:16 +0900

    http요청 servlet으로 처리하고 jsp 뷰 템플릿사용하여 화면에 뿌려주기

---
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -0,0 +1,35 @@
+package hello.servlet.web.servletmvc;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        final String username = request.getParameter("username");
+        final int age = Integer.parseInt(request.getParameter("age"));
+
+        final Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관한다.
+        request.setAttribute("member", member);
+
+        final String viewPath = "/WEB-INF/views/save-result.jsp";
+        final RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,47 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        final Member member = new Member("hello", 20);
+        //when
+        final Member saveMember = memberRepository.save(member);
+        //then
+        final Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(findMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        final Member member1 = new Member("hello", 20);
+        final Member member2 = new Member("hello2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        final List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
