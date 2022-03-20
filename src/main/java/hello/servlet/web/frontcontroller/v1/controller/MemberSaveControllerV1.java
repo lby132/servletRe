56efa2a836715d2f4efa48c08b24c6efd22476fc
lby132 <quddbscjswow@naver.com>
2022-03-21 00:43:36 +0900

    FrontController로 http요청 처리

---
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberSaveControllerV1.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.frontcontroller.v1.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberSaveControllerV1 implements ControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        final String username = request.getParameter("username");
+        final int age = Integer.parseInt(request.getParameter("age"));
+
+        final Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        //모델에 데이터를 보관한다. (Http 메세지에 데이터를 보관할수있는 작은 공간이 있어서 setAttribute 로 값을 저장할 수 있다. 이게 모델 역할을 해준다.)
+        request.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        final RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
